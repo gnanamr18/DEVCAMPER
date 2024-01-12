54d4fb6ee2931918abfe72751d0da47dc6163693
gnanamr18 <gnanam.r18@gmail.com>
2024-01-12 11:19:14 +0530

    finished upto routes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bootcamps = require("./Routes/bootcamps");
 
 //Load env var
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
